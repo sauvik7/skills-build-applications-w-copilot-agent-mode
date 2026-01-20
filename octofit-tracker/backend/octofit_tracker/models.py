be4e6203c13c8c9826faf2eaf68d4380fc1c2459
sauvik7 <sauvik.roy@zensar.com>
2026-01-20 11:43:53 +0000

    Add populate_db command to seed database with test data

--- octofit-tracker/backend/octofit_tracker/models.py
+++ octofit-tracker/backend/octofit_tracker/models.py
@@ -1,12 +1,15 @@
 from djongo import models
+from bson import ObjectId
 
 class Team(models.Model):
+    id = models.ObjectIdField(primary_key=True, default=ObjectId, editable=False)
     name = models.CharField(max_length=100, unique=True)
     description = models.TextField(blank=True)
     def __str__(self):
         return self.name
 
 class User(models.Model):
+    id = models.ObjectIdField(primary_key=True, default=ObjectId, editable=False)
     email = models.EmailField(unique=True)
     name = models.CharField(max_length=100)
     team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='members')
@@ -15,6 +18,7 @@ class User(models.Model):
         return self.name
 
 class Activity(models.Model):
+    id = models.ObjectIdField(primary_key=True, default=ObjectId, editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='activities')
     type = models.CharField(max_length=100)
     duration = models.IntegerField()  # in minutes
@@ -23,6 +27,7 @@ class Activity(models.Model):
         return f"{self.user.name} - {self.type}"
 
 class Workout(models.Model):
+    id = models.ObjectIdField(primary_key=True, default=ObjectId, editable=False)
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     suggested_for = models.ManyToManyField(Team, related_name='workouts')
@@ -30,6 +35,7 @@ class Workout(models.Model):
         return self.name
 
 class Leaderboard(models.Model):
+    id = models.ObjectIdField(primary_key=True, default=ObjectId, editable=False)
     team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='leaderboards')
     score = models.IntegerField(default=0)
     def __str__(self):
